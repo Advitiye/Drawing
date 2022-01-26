2891754e26102f3bd9b29abf727336fcf944d853
Advitiye <84700828+Advitiye@users.noreply.github.com>
2022-01-26 16:39:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+noseX = 0
+noseY = 0
+difference = 0
+right_wrist = 0
+left_wrist = 0
+
+function setup() {
+    video = createCapture(VIDEO)
+    video.size(400,400)
+
+    canvas = createCanvas(400,400)
+    canvas.position(800,200)
+
+    modpos = ml5.poseNet(video)
+    modpos.on("pose",gotPoses)
+}
+function gotPoses(results) {
+    if(results.length>0) {
+    noseX = results[0].pose.nose.x
+    noseY = results[0].pose.nose.y
+
+    right_wrist = results[0].pose.leftWrist.x
+    left_wrist = results[0].pose.rightWrist.x
+    
+    difference = floor(right_wrist - left_wrist)
+    }
+}
+function draw() {
+    background("#82a19a")
+    fill("#cf933a")
+    stroke("#cf933a")
+
+    square(noseX,noseY,difference)
+    
+    document.getElementById("hewid").innerHTML = difference
+}
